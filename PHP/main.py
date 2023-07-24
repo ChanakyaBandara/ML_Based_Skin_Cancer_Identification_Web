b09f1b20cb83266dcfa09590af6181c27a39b9b5
ChanakyaBandara5 <chanakya@emageia.com>
2023-07-24 09:08:16 +0530

    first commit

---
+++ PHP/main.py
@@ -0,0 +1,34 @@
+import tensorflow.keras
+import numpy as np
+import cv2
+import sys
+import json,codecs
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = tensorflow.keras.models.load_model('SkinCancer_CNN.h5')
+
+def detection(filename):
+    print(filename)
+    image_size = 32
+
+    img = cv2.imread("uploads/"+filename)
+
+    if img is None:
+        print(f"Error loading image: {filename}")
+
+    img = cv2.resize(img, (image_size, image_size))
+    img = img / 255.0
+    prediction = model.predict(np.array([img]))
+
+    # print(prediction)
+    json.dump(prediction.tolist(), codecs.open("result.json", 'w', encoding='utf-8'),
+              separators=(',', ':'),
+              sort_keys=True,
+              indent=4)
+
+if __name__ == '__main__':
+    detection(str(sys.argv[1]))
+
---
+++ js/main.js
@@ -0,0 +1,64 @@
+var jsonData;
+
+$.getJSON("./js/info.json", function (json) {
+    jsonData = json;
+});
+
+$("form#fileForm").on("submit", function (e) {
+    console.log(e)
+    e.preventDefault();
+    var formData = new FormData(this);
+    console.log(formData)
+    Spinner();
+    Spinner.show();
+    $.ajax({
+        url: "PHP/backend.php",
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        contentType: false,
+        cache: false,
+        processData: false,
+    }).done(function (result) {
+        Spinner.hide();
+        console.log(result);
+        processResult = processData(result.result);
+        populateTable(processResult);
+    })
+});
+
+function processData(result) {
+    var selectedIndex;
+    var selectedAccuracy = 0;
+    var finalResult = [];
+    result.forEach((accuracy, index) => {
+        if (accuracy > 0.7 && accuracy > selectedAccuracy) {
+            selectedIndex = index;
+            selectedAccuracy = accuracy;
+        }
+        finalResult.push({ index, accuracy, name: jsonData[index].name, description: jsonData[index].description })
+    });
+    document.getElementById('cancerName').innerHTML = finalResult[selectedIndex].name
+    document.getElementById('cancerAccuracy').innerHTML = "Accuracy : " + finalResult[selectedIndex].accuracy*100 + " %"
+    document.getElementById('cancerDescription').innerHTML = "Description : " + finalResult[selectedIndex].description
+    return finalResult;
+}
+
+function populateTable(data){
+    $("#resultTbl").empty();
+    $("#resultTbl").append(
+      "<thead><th>Index</th><th>Name</th><th>Accuracy</th></thead><tbody>"
+    );
+    data.forEach(row => {
+        $("#resultTbl").append(
+            "<tr><td>" +
+              row.index +
+              "</td><td>" +
+              row.name +
+              "</td><td>" +
+              row.accuracy +
+              "</td></tr>"
+          );
+    });
+    $("#resultTbl").append("</tbody>");
+}
